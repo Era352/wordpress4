38effce8feae20e9c39a5005e46044a83afafaf0
Era352 <era.kryeziu787@gmail.com>
2025-03-06 19:14:53 +0100

    Add files via upload

---
+++ ds-theme-final/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<header>
+        <section class="top-bar">
+            <div class="logo">
+                Logo
+            </div>
+            <div class="searchbox">
+                Search
+            </div>
+        </section>
+        <section class="menu-area">
+            <nav class="main-menu">
+                Menu
+            </nav>
+        </section>
+    </header>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ ds-theme-final/index.php
@@ -0,0 +1,21 @@
+
+    <!--Hero-->
+
+    <?php get_header();?>
+    <div id="content" class="site-content">
+        <div id="primary" class="cotent-area">
+            <main id="main" class="site-main">
+                <section class="hero">
+                    Hero
+                </section>
+                <section class="services">
+                    Services
+                </section>
+                <section class="home-block">
+                    Blog
+                </section>
+            </main>
+        </div>
+    </div>
+    <!---->
+    <?php get_footer();?>
